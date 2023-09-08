e36dc7bee55616d02723f2bf3d66d9a0511ce57c
Tieen001 <134282928+Tieen001@users.noreply.github.com>
2023-09-08 16:24:41 +0700

    [SQL] Update table

--- table_sql.sql
+++ table_sql.sql
@@ -208,32 +208,13 @@ CREATE TABLE import_receipt(
 go
 CREATE TABLE import_receipt_detail(
 	import_receipt_detail_id BIGINT PRIMARY KEY IDENTITY(1,1),
-	product_id BIGINT,
+	product_detail_id BIGINT,
 	import_receipt_id BIGINT,
 	quantity INT,
 	price FLOAT,
 	deleted BIT NOT NULL DEFAULT 0,
 	CONSTRAINT fk_import_receipt_detail_import FOREIGN KEY (import_receipt_id) REFERENCES import_receipt(import_receipt_id),
-	CONSTRAINT fk_import_receipt_detale_product FOREIGN KEY (product_id) REFERENCES product(product_id)
-)
-go
-CREATE TABLE discount(
-	discount_id BIGINT PRIMARY KEY IDENTITY(1,1),
-	discount_name NVARCHAR(100),
-	[description] NVARCHAR(250),
-	[start_date] DATETIME,
-	end_date DATETIME,
-	discount_rate DECIMAL(5,2),
-	display BIT,
-	[status] NVARCHAR(100)
-)
-go
-CREATE TABLE product_discount(
-	product_discount_id BIGINT PRIMARY KEY IDENTITY(1,1),
-	discount_id BIGINT,
-	product_id BIGINT,
-	CONSTRAINT fk_product_discount_discount FOREIGN KEY (discount_id) REFERENCES discount(discount_id),
-	CONSTRAINT fk_product_discount_product FOREIGN KEY (product_id) REFERENCES product(product_id)
+	CONSTRAINT fk_import_receipt_detale_product FOREIGN KEY (product_detail_id) REFERENCES product_detail(product_detail_id)
 )
 go
 CREATE TABLE direct_discount(
