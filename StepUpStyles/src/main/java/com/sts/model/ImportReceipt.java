d42e50a85f500644b35af286b23887eb7da9362e
Tieen001 <134282928+Tieen001@users.noreply.github.com>
2023-09-10 15:45:38 +0700

    [ECLIPSE] Update model

--- StepUpStyles/src/main/java/com/sts/model/ImportReceipt.java
+++ StepUpStyles/src/main/java/com/sts/model/ImportReceipt.java
@@ -2,7 +2,11 @@ package com.sts.model;
 
 import java.io.Serializable;
 import javax.persistence.*;
-import java.sql.Timestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 
@@ -10,6 +14,9 @@ import java.util.List;
  * The persistent class for the import_receipt database table.
  * 
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name="import_receipt")
 @NamedQuery(name="ImportReceipt.findAll", query="SELECT i FROM ImportReceipt i")
@@ -18,15 +25,15 @@ public class ImportReceipt implements Serializable {
 
 	@Id
 	@Column(name="import_receipt_id")
-	private long importReceiptId;
+	private Long importReceiptId;
 
-	private boolean deleted;
+	private Boolean deleted;
 
 	@Column(name="import_date")
-	private Timestamp importDate;
+	private String importDate;
 
 	@Column(name="total_amount")
-	private double totalAmount;
+	private Double totalAmount;
 
 	//bi-directional many-to-one association to Supplier
 	@ManyToOne
@@ -42,64 +49,7 @@ public class ImportReceipt implements Serializable {
 	@OneToMany(mappedBy="importReceipt")
 	private List<ImportReceiptDetail> importReceiptDetails;
 
-	public ImportReceipt() {
-	}
-
-	public long getImportReceiptId() {
-		return this.importReceiptId;
-	}
-
-	public void setImportReceiptId(long importReceiptId) {
-		this.importReceiptId = importReceiptId;
-	}
-
-	public boolean getDeleted() {
-		return this.deleted;
-	}
-
-	public void setDeleted(boolean deleted) {
-		this.deleted = deleted;
-	}
-
-	public Timestamp getImportDate() {
-		return this.importDate;
-	}
-
-	public void setImportDate(Timestamp importDate) {
-		this.importDate = importDate;
-	}
-
-	public double getTotalAmount() {
-		return this.totalAmount;
-	}
-
-	public void setTotalAmount(double totalAmount) {
-		this.totalAmount = totalAmount;
-	}
-
-	public Supplier getSupplier() {
-		return this.supplier;
-	}
-
-	public void setSupplier(Supplier supplier) {
-		this.supplier = supplier;
-	}
-
-	public User getUser() {
-		return this.user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public List<ImportReceiptDetail> getImportReceiptDetails() {
-		return this.importReceiptDetails;
-	}
-
-	public void setImportReceiptDetails(List<ImportReceiptDetail> importReceiptDetails) {
-		this.importReceiptDetails = importReceiptDetails;
-	}
+	
 
 	public ImportReceiptDetail addImportReceiptDetail(ImportReceiptDetail importReceiptDetail) {
 		getImportReceiptDetails().add(importReceiptDetail);
