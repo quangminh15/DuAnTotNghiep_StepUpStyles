a69ed83b35d3339ae5dd21a8795c91925023937d
HAI <volonghai1511@gmail.com>
2023-09-10 19:59:16 +0700

    [JAVA]_Chỉnh sửa kiều dữ liệu các bảng
    +Cart
    +Cart_detail
    +Order
    +Order_detail
    +payment_method
    +Shipping_address

--- StepUpStyles/src/main/java/com/sts/model/ShippingAddress.java
+++ StepUpStyles/src/main/java/com/sts/model/ShippingAddress.java
@@ -2,6 +2,11 @@ package com.sts.model;
 
 import java.io.Serializable;
 import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
 
@@ -11,6 +16,9 @@ import java.util.List;
  */
 @Entity
 @Table(name="shipping_address")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @NamedQuery(name="ShippingAddress.findAll", query="SELECT s FROM ShippingAddress s")
 public class ShippingAddress implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -20,16 +28,16 @@ public class ShippingAddress implements Serializable {
 	private long shippingAddressId;
 
 	@Column(name="address_details")
-	private Object addressDetails;
+	private String addressDetails;
 
 	@Column(name="default_address")
 	private boolean defaultAddress;
 
-	private Object district;
+	private String district;
 
-	private Object province;
+	private String province;
 
-	private Object ward;
+	private String ward;
 
 	//bi-directional many-to-one association to Order
 	@OneToMany(mappedBy="shippingAddress")
@@ -40,85 +48,85 @@ public class ShippingAddress implements Serializable {
 	@JoinColumn(name="users_id")
 	private User user;
 
-	public ShippingAddress() {
-	}
+	// public ShippingAddress() {
+	// }
 
-	public long getShippingAddressId() {
-		return this.shippingAddressId;
-	}
+	// public long getShippingAddressId() {
+	// 	return this.shippingAddressId;
+	// }
 
-	public void setShippingAddressId(long shippingAddressId) {
-		this.shippingAddressId = shippingAddressId;
-	}
+	// public void setShippingAddressId(long shippingAddressId) {
+	// 	this.shippingAddressId = shippingAddressId;
+	// }
 
-	public Object getAddressDetails() {
-		return this.addressDetails;
-	}
+	// public Object getAddressDetails() {
+	// 	return this.addressDetails;
+	// }
 
-	public void setAddressDetails(Object addressDetails) {
-		this.addressDetails = addressDetails;
-	}
+	// public void setAddressDetails(Object addressDetails) {
+	// 	this.addressDetails = addressDetails;
+	// }
 
-	public boolean getDefaultAddress() {
-		return this.defaultAddress;
-	}
+	// public boolean getDefaultAddress() {
+	// 	return this.defaultAddress;
+	// }
 
-	public void setDefaultAddress(boolean defaultAddress) {
-		this.defaultAddress = defaultAddress;
-	}
+	// public void setDefaultAddress(boolean defaultAddress) {
+	// 	this.defaultAddress = defaultAddress;
+	// }
 
-	public Object getDistrict() {
-		return this.district;
-	}
+	// public Object getDistrict() {
+	// 	return this.district;
+	// }
 
-	public void setDistrict(Object district) {
-		this.district = district;
-	}
+	// public void setDistrict(Object district) {
+	// 	this.district = district;
+	// }
 
-	public Object getProvince() {
-		return this.province;
-	}
+	// public Object getProvince() {
+	// 	return this.province;
+	// }
 
-	public void setProvince(Object province) {
-		this.province = province;
-	}
+	// public void setProvince(Object province) {
+	// 	this.province = province;
+	// }
 
-	public Object getWard() {
-		return this.ward;
-	}
+	// public Object getWard() {
+	// 	return this.ward;
+	// }
 
-	public void setWard(Object ward) {
-		this.ward = ward;
-	}
+	// public void setWard(Object ward) {
+	// 	this.ward = ward;
+	// }
 
-	public List<Order> getOrders() {
-		return this.orders;
-	}
+	// public List<Order> getOrders() {
+	// 	return this.orders;
+	// }
 
-	public void setOrders(List<Order> orders) {
-		this.orders = orders;
-	}
+	// public void setOrders(List<Order> orders) {
+	// 	this.orders = orders;
+	// }
 
-	public Order addOrder(Order order) {
-		getOrders().add(order);
-		order.setShippingAddress(this);
+	// public Order addOrder(Order order) {
+	// 	getOrders().add(order);
+	// 	order.setShippingAddress(this);
 
-		return order;
-	}
+	// 	return order;
+	// }
 
-	public Order removeOrder(Order order) {
-		getOrders().remove(order);
-		order.setShippingAddress(null);
+	// public Order removeOrder(Order order) {
+	// 	getOrders().remove(order);
+	// 	order.setShippingAddress(null);
 
-		return order;
-	}
+	// 	return order;
+	// }
 
-	public User getUser() {
-		return this.user;
-	}
+	// public User getUser() {
+	// 	return this.user;
+	// }
 
-	public void setUser(User user) {
-		this.user = user;
-	}
+	// public void setUser(User user) {
+	// 	this.user = user;
+	// }
 
 }
