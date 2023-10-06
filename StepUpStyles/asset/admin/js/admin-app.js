0c14c78de2f0bcbecf71f078ce3236a3067151d5
quangminh15 <phamm7696@gmail.com>
2023-10-06 21:17:56 +0700

    đổi layout admin

---
+++ StepUpStyles/asset/admin/js/admin-app.js
@@ -0,0 +1,93 @@
+app = angular.module("admin-app", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+	$routeProvider
+		.when("/indexAdmin", {
+			templateUrl: "/templates/admins/index.html",
+			controller: "indexAdmin-ctrl"
+		})
+		.when("/product", {
+			templateUrl: "/templates/admins/products/product.html",
+			controller: "product-ctrl"
+		})
+		.when("/productImage", {
+			templateUrl: "/templates/admins/products/productimage.html",
+			controller: "productimage-ctrl"
+		})
+		.when("/productsize", {
+			templateUrl: "/templates/admins/products/size.html",
+			controller: "size-ctrl"
+		})
+		.when("/brand", {
+			templateUrl: "/templates/admins/products/brand.html",
+			controller: "brand-ctrl"
+		})
+		.when("/category", {
+			templateUrl: "/templates/admins/products/category.html",
+			controller: "category-ctrl"
+		})
+		.when("/subCategory", {
+			templateUrl: "/templates/admins/products/subcategory.html",
+			controller: "subcategory-ctrl"
+		})
+		.when("/productDetail", {
+			templateUrl: "/templates/admins/products/productdetail.html",
+			controller: "productdetail-ctrl"
+		})
+		.when("/color", {
+			templateUrl: "/templates/admins/products/color.html",
+			controller: "color-ctrl"
+		})
+		.when("/review", {
+			templateUrl: "/templates/admins/reviews/review.html",
+			controller: "review-ctrl"
+		})
+		.when("/order", {
+			templateUrl: "/templates/admins/orders/order.html",
+			controller: "order-ctrl"
+		})
+		.when("/directDiscount", {
+			templateUrl: "/templates/admins/discounts/directdiscount.html",
+			controller: "directdiscount-ctrl"
+		})
+		.when("/discount", {
+			templateUrl: "/templates/admins/discounts/discount.html",
+			controller: "discount-ctrl"
+		}).when("/discountProd", {
+			templateUrl: "/templates/admins/discounts/productdiscount.html",
+			controller: "prodsDiscount-ctrl"
+		})
+		.when("/discountCate", {
+			templateUrl: "/templates/admins/discounts/categorydiscount.html",
+			controller: "catesDiscount-ctrl"
+		})
+		.when("/supplier", {
+			templateUrl: "/templates/admins/suppliers/supplier.html",
+			controller: "supplier-ctrl"
+		})
+		.when("/ImportReceipt", {
+			templateUrl: "/templates/admins/ImportReceipt/ImportReceipts.html",
+			controller: "ImportReceipt-ctrl"
+		})
+		.when("/ImportReceiptDetail", {
+			templateUrl: "/templates/admins/ImportReceipt/ImportReceiptDetail.html",
+			controller: "ImportReceiptDetail-ctrl"
+		})
+		.when("/user", {
+			templateUrl: "/templates/admins/user/user.html",
+			controller: "user-ctrl"
+		})
+		.when("/productOFcategory", {
+			templateUrl: "/templates/admins/statistical/productofcategory.html",
+			controller: "prodofcate-ctrl"
+		})
+		.when("/totalproductrating", {
+			templateUrl: "/templates/admins/statistical/totalproductofrating.html",
+			controller: "totalproductrating-ctrl"
+		})
+		.otherwise({
+			templateUrl: "/templates/admins/index.html",
+			controller: "indexAdmin-ctrl"
+		});
+})
+
--- StepUpStyles/src/main/java/com/sts/config/Config.java
+++ StepUpStyles/src/main/java/com/sts/config/Config.java
@@ -1,6 +1,8 @@
 package com.sts.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +12,11 @@ public class Config implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**","/templates/**")
         .addResourceLocations("classpath:/static/","classpath:/templates/");
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring()
+            .antMatchers("/asset/**");
     }
 }
