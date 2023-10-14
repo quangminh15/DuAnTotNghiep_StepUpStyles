90a51e2a96b73292d7bbeea578ad8e996003a064
quangminh15 <phamm7696@gmail.com>
2023-10-14 21:21:33 +0700

    Hoàn thành phân trang sản phẩm

--- StepUpStyles/src/main/java/com/sts/controller/ProductController.java
+++ StepUpStyles/src/main/java/com/sts/controller/ProductController.java
@@ -1,21 +1,25 @@
 package com.sts.controller;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.sts.dao.ProductDAO;
 import com.sts.model.Brand;
 import com.sts.model.Category;
 import com.sts.model.Color;
 import com.sts.model.Favorite;
 import com.sts.model.Product;
 import com.sts.model.Size;
-import com.sts.model.DTO.CategoryProductCountDTO;
 import com.sts.service.BrandService;
 import com.sts.service.CategoryService;
 import com.sts.service.ColorService;
@@ -37,7 +41,7 @@ public class ProductController {
 
 	@Autowired
 	ColorService colorService;
-	
+
 	@Autowired
 	BrandService brandService;
 
@@ -45,19 +49,32 @@ public class ProductController {
 	CategoryService categoryService;
 
 	@Autowired
-    FavoriteService favoriteService;
+	ProductDAO pdao;
+
+	@Autowired
+	FavoriteService favoriteService;
 
 	// Trang sản phẩm
 	@RequestMapping("/list_products")
-	public String listproducts(Model model) {
-		List<Product> productss = productservice.loadAllNoDeletedAndActivitiesTrue();
-		model.addAttribute("productitems", productss);
+	public String listproducts(Model model, @RequestParam("page") Optional<Integer> page,
+			@RequestParam("cid") Optional<Integer> cid,@RequestParam("bid") Optional<Integer> bid) {
+//		List<Product> productss = productservice.loadAllNoDeletedAndActivitiesTrue();
+//		model.addAttribute("productitems", productss);
+
+		Pageable pageable = PageRequest.of(page.orElse(0), 6);
+		Page<Product> products = handleOtherParams(pageable, cid, bid);
+
+		var numberOfPages = products.getTotalPages();
+		model.addAttribute("currIndex", page.orElse(0));
+		model.addAttribute("numberOfPages", numberOfPages);
+
+		model.addAttribute("productitems", products);
 
 		List<Category> categories = categoryService.loadAllNoDeletedAndActivitiesTrue();
 		List<Brand> brands = brandService.loadAllNoDeletedAndActivitiesTrue();
 		List<Color> colors = colorService.loadAllNoDeletedAndActivitiesTrue();
 		List<Size> sizes = sizeService.loadAllNoDeletedAndActivitiesTrue();
-		
+
 		// Tạo danh sách danh mục kèm số lượng sản phẩm
 //		List<CategoryProductCountDTO> categoryProductCounts = new ArrayList<>();
 //		for (Category category : categories) {
@@ -73,8 +90,18 @@ public class ProductController {
 		model.addAttribute("sizes", sizes);
 
 		List<Favorite> item = favoriteService.findByUserId(3);
-        model.addAttribute("favoriteitems", item);
+		model.addAttribute("favoriteitems", item);
 		return "users/list_products";
+	}
+
+	private Page<Product> handleOtherParams(Pageable pageable, Optional<Integer> cid, Optional<Integer> bid) {
+		if (cid.isPresent()) {
+			return productservice.findByCategoryIDPaged(cid.get(), pageable);
+		} else if (bid.isPresent()) {
+			return productservice.findByBrandIDPaged(bid.get(), pageable);
+		}  
+
+		return productservice.loadAllNoDeletedAndActivitiesTrue(pageable);
 	}
 
 	// Trang sản phẩm chi tiết
--- StepUpStyles/src/main/java/com/sts/dao/ProductDAO.java
+++ StepUpStyles/src/main/java/com/sts/dao/ProductDAO.java
@@ -3,7 +3,7 @@ package com.sts.dao;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,29 +11,42 @@ import org.springframework.data.repository.query.Param;
 import com.sts.model.Category;
 import com.sts.model.Product;
 
-public interface ProductDAO extends JpaRepository<Product, Integer>{
+public interface ProductDAO extends JpaRepository<Product, Integer> {
 	@Query("SELECT c FROM Product c WHERE c.productName LIKE %:keyword%")
 	List<Product> findByProductNameContaining(@Param("keyword") String keyword);
-	
+
 	@Query("SELECT c FROM Product c WHERE c.deleted = true")
 	List<Product> loadAllDeleted();
 
 	@Query("SELECT c FROM Product c WHERE c.deleted = false")
 	List<Product> loadAllNoDeleted();
-	
-	@Query("SELECT c FROM Product c WHERE c.deleted = false and c.activities = true")
-	List<Product> loadAllNoDeletedAndActivitiesTrue();
+
+//	@Query("SELECT c FROM Product c WHERE c.deleted = false and c.activities = true")
+//	List<Product> loadAllNoDeletedAndActivitiesTrue();
 
 	Long countByCategory(Category category);
-	
-	//Đếm số lượng sản phẩm để hiển thị lên danh mục người dùng
+
+	// Đếm số lượng sản phẩm để hiển thị lên danh mục người dùng
 	@Query("SELECT COUNT(p) FROM Product p WHERE p.category = :category AND p.activities = :activities AND p.deleted = :deleted")
-	Long countProductsByCategoryWithConditions(@Param("category") Category category, @Param("activities") Boolean activities, @Param("deleted") Boolean deleted);
+	Long countProductsByCategoryWithConditions(@Param("category") Category category,
+			@Param("activities") Boolean activities, @Param("deleted") Boolean deleted);
 
-	
 	// Truy vấn tùy chỉnh để đếm sản phẩm theo danh mục
-    @Query("SELECT COUNT(p) FROM Product p WHERE p.category = :category")
-    Long countProductsByCategory(@Param("category") Category category);
+	@Query("SELECT COUNT(p) FROM Product p WHERE p.category = :category")
+	Long countProductsByCategory(@Param("category") Category category);
 
-	 List<Product> findByCategory(Category category);
+	List<Product> findByCategory(Category category);
+
+	// Phân trang
+	@Query("SELECT c FROM Product c WHERE c.deleted = false and c.activities = true")
+	Page<Product> loadAllNoDeletedAndActivitiesTrue(Pageable pageable);
+
+	@Query("select p from Product p where p.category.categoryID=?1")
+	Page<Product> findByCategoryIDPaged(Integer cid, Pageable pageable);
+
+	@Query("select p from Product p where p.brand.brandID=?1 and p.deleted = false and p.activities = true")
+	Page<Product> findByBrandID(Integer bid, Pageable pageable);
+	
+	@Query("select p from Product p where p.deleted = false and p.activities = true and lower(p.productName) like lower(concat('%', ?1, '%'))")
+    Page<Product> findByProductNameContaining(String keyword, Pageable pageable);
 }
